b7418a85bd4f56501de064dc2a9863a20118a007
Aditya Gautam Mishra <adityamishr123@gmail.com>
2020-07-21 13:55:38 +0530

    generated pom from start.spring.io
    pom generation

---
+++ MediKalStore/src/main/java/com/example/MediKalStore/MediKalStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.MediKalStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediKalStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediKalStoreApplication.class, args);
+	}
+
+}
---
+++ MediKalStore/src/test/java/com/example/MediKalStore/MediKalStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MediKalStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediKalStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
